4d37f7edf63cb8c40c780dd578ec7c409a1a3f02
ORTIZ <aidenortiz@mail.ru>
2022-07-14 00:08:38 +0300

    js - добавлен script.js
    py - Добавлен django.py
    
    @AidenOrtiz

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+console.log("Hello, and welcome to Hanvesting Web-Site.");
+console.log("Checking web-site status....");
+console.log("Site is working!");
+console.log("Made by ORTIZ, 2022.");
+console.log("https://github.com/AidenOrtiz");
---
+++ python/django.py
@@ -0,0 +1,2 @@
+def MySite():
+    None
